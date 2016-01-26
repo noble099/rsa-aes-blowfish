35bd6fa47a318a83d42164c204cf4be673db8a9b
Alif Jamaluddin <alifjamaluddin92@gmail.com>
2016-01-26 22:54:01 +0800

    Done basic encryption for text string in RSA

---
+++ src/fileencryption/Main.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileencryption;
+
+
+import static fileencryption.RSA.areKeysPresent;
+import static fileencryption.RSA.decrypt;
+import static fileencryption.RSA.encrypt;
+import static fileencryption.RSA.generateKey;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author megasap
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+     /**
+   * Test the EncryptionUtil
+   */
+  public static final String PRIVATE_KEY_FILE = "/Users/megasap/Documents/private.key";
+  public static final String PUBLIC_KEY_FILE = "/Users/megasap/Documents/public.key";
+
+  public static void main(String[] args) {
+
+    try {
+
+      // Check if the pair of keys are present else generate those.
+      if (!areKeysPresent(PRIVATE_KEY_FILE, PUBLIC_KEY_FILE)) {
+        // Method generates a pair of keys using the RSA algorithm and stores it
+        // in their respective files
+        generateKey(PRIVATE_KEY_FILE, PUBLIC_KEY_FILE);
+      }
+
+      final String originalText = "Text to be encrypted ";
+      
+      byte[] encryptedFile = encryptRSA(originalText,PUBLIC_KEY_FILE);      
+      decryptRSA(encryptedFile,PRIVATE_KEY_FILE);
+
+     
+
+      
+
+      // Printing the Original, Encrypted and Decrypted Text
+      
+      
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+    
+  
+  public static byte[] encryptRSA(String plainText, String publicKeyDir){
+      ObjectInputStream inputStream = null;
+      byte[] encryptedText = null;
+      try {
+          // Encrypt the string using the public key
+          inputStream = new ObjectInputStream(new FileInputStream(PUBLIC_KEY_FILE));
+          final PublicKey publicKey = (PublicKey) inputStream.readObject();
+          final byte[] cipherText = encrypt(plainText, publicKey);
+          System.out.println("Encrypted: " +cipherText.toString());
+          encryptedText = cipherText;
+      } catch (Exception e) {
+          e.printStackTrace();
+      } 
+      return encryptedText;
+  }
+  
+  public static void decryptRSA(byte[] cipherText, String privateKeyDir){
+      ObjectInputStream inputStream = null;
+      try {
+          // Decrypt the cipher text using the private key.
+          inputStream = new ObjectInputStream(new FileInputStream(privateKeyDir));
+          final PrivateKey privateKey = (PrivateKey) inputStream.readObject();
+          final String plainText = decrypt(cipherText, privateKey);
+          System.out.println("Decrypted: " + plainText);
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      
+  }
+}
---
+++ src/fileencryption/RSA.java
@@ -0,0 +1,152 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author megasap
+ */
+
+package fileencryption;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+
+
+public class RSA {
+
+  /**
+   * String to hold name of the encryption algorithm.
+   */
+  public static final String ALGORITHM = "RSA";
+
+
+  /**
+   * Generate key which contains a pair of private and public key using 1024
+   * bytes. Store the set of keys in Prvate.key and Public.key files.
+   * 
+   * @throws NoSuchAlgorithmException
+   * @throws IOException
+   * @throws FileNotFoundException
+   */
+  public static void generateKey(String privateKeyDir, String publicKeyDir) {
+    try {
+      final KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+      keyGen.initialize(1024);
+      final KeyPair key = keyGen.generateKeyPair();
+
+      File privateKeyFile = new File(privateKeyDir);
+      File publicKeyFile = new File(publicKeyDir);
+
+      // Create files to store public and private key
+      if (privateKeyFile.getParentFile() != null) {
+        privateKeyFile.getParentFile().mkdirs();
+      }
+      privateKeyFile.createNewFile();
+
+      if (publicKeyFile.getParentFile() != null) {
+        publicKeyFile.getParentFile().mkdirs();
+      }
+      publicKeyFile.createNewFile();
+
+      // Saving the Public key in a file
+      ObjectOutputStream publicKeyOS = new ObjectOutputStream(
+          new FileOutputStream(publicKeyFile));
+      publicKeyOS.writeObject(key.getPublic());
+      publicKeyOS.close();
+
+      // Saving the Private key in a file
+      ObjectOutputStream privateKeyOS = new ObjectOutputStream(
+          new FileOutputStream(privateKeyFile));
+      privateKeyOS.writeObject(key.getPrivate());
+      privateKeyOS.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  /**
+   * The method checks if the pair of public and private key has been generated.
+   * 
+   * @return flag indicating if the pair of keys were generated.
+   */
+  public static boolean areKeysPresent(String privateKeyDir, String publicKeyDir) {
+
+    File privateKey = new File(privateKeyDir);
+    File publicKey = new File(publicKeyDir);
+
+    if (privateKey.exists() && publicKey.exists()) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Encrypt the plain text using public key.
+   * 
+   * @param text
+   *          : original plain text
+   * @param key
+   *          :The public key
+   * @return Encrypted text
+   * @throws java.lang.Exception
+   */
+  public static byte[] encrypt(String text, PublicKey key) {
+    byte[] cipherText = null;
+    try {
+      // get an RSA cipher object and print the provider
+      final Cipher cipher = Cipher.getInstance(ALGORITHM);
+      // encrypt the plain text using the public key
+      cipher.init(Cipher.ENCRYPT_MODE, key);
+      cipherText = cipher.doFinal(text.getBytes());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return cipherText;
+  }
+
+  /**
+   * Decrypt text using private key.
+   * 
+   * @param text
+   *          :encrypted text
+   * @param key
+   *          :The private key
+   * @return plain text
+   * @throws java.lang.Exception
+   */
+  public static String decrypt(byte[] text, PrivateKey key) {
+    byte[] dectyptedText = null;
+    try {
+      // get an RSA cipher object and print the provider
+      final Cipher cipher = Cipher.getInstance(ALGORITHM);
+
+      // decrypt the text using the private key
+      cipher.init(Cipher.DECRYPT_MODE, key);
+      dectyptedText = cipher.doFinal(text);
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
+    return new String(dectyptedText);
+  }
+
+
+}
